73917c1d9ba5f180c87108de9068312de6d17a9e
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-10-09 21:04:25 -0500

    delete author and authors books

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -3,13 +3,22 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
+import { deleteSingleAuthor } from '../api/authorData';
+import { deleteAuthorBooks } from '../api/mergedData';
 
-function AuthorCard({ authorObj }) {
+function AuthorCard({ authorObj, onUpdate }) {
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name} ${authorObj.last_name}?`)) {
+      deleteSingleAuthor(authorObj.firebaseKey).then(() => onUpdate());
+      deleteAuthorBooks(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Body>
         <Card.Title>{authorObj?.first_name} {authorObj?.last_name}</Card.Title>
-        <p className="card-text bold">{authorObj?.favorite && <span>FAVORITE<br /></span> } </p>
+        <p className="card-text bold">{authorObj?.favorite && <span>💛<br /></span> } </p>
         <p className="card-text bold">{authorObj?.email}</p>
         {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
         <Link href={`/author/${authorObj?.firebaseKey}`} passHref>
@@ -19,9 +28,9 @@ function AuthorCard({ authorObj }) {
         <Link href={`/author/edit/${authorObj?.firebaseKey}`} passHref>
           <Button variant="info">EDIT</Button>
         </Link>
-        {/* <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
           DELETE
-        </Button> */}
+        </Button>
       </Card.Body>
     </Card>
   );
@@ -35,7 +44,7 @@ AuthorCard.propTypes = {
     email: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
-  // onUpdate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default AuthorCard;
